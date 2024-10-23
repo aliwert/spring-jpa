a39e3e823e3d2a643108c7ee51cacc85c16d6937
aliwert <154356044+aliwert@users.noreply.github.com>
2024-10-23 16:50:10 +0300

    Initial commit

---
+++ src/main/java/com/alimert/starter/SpringDataJpaApplication.java
@@ -0,0 +1,15 @@
+package com.alimert.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan(basePackages = {"com.alimert"})
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+}
